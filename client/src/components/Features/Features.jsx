69cbe7f071f0ced8acfd428d42f48d27803c773d
E-Shangeraev <e.shangeraev@gmail.com>
2021-06-09 10:29:48 +0700

    Добавляю изображения в блок Features на главной странице

--- client/src/components/Features/Features.jsx
+++ client/src/components/Features/Features.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import img1 from '@assets/img/Main/1.jpg'
+import imageJpg1 from '@assets/img/Main/Features/1.jpg'
+import imageWebp1 from '@assets/img/Main/Features/1.webp'
+import imageJpg2 from '@assets/img/Main/Features/2.jpg'
+import imageWebp2 from '@assets/img/Main/Features/2.webp'
+import imageJpg3 from '@assets/img/Main/Features/3.jpg'
+import imageWebp3 from '@assets/img/Main/Features/3.webp'
 
 const Features = () => (
   <section className="features">
     <ul className="features__list">
       <li className="pl pr">
-        <img src={img1} alt="новый спротивный зал" />
+        <picture className="about__image">
+          <source srcSet={imageWebp1} type="image/webp" />
+          <img src={imageJpg1} alt="новый спротивный зал" />
+        </picture>
         <h3 className="subtitle">новый спротивный зал</h3>
         <p>
           <span>Плошадь - 250м2</span>
@@ -13,7 +21,10 @@ const Features = () => (
         </p>
       </li>
       <li className="pl pr">
-        <img src={img1} alt="профессиональное оборудование" />
+        <picture className="about__image">
+          <source srcSet={imageWebp2} type="image/webp" />
+          <img src={imageJpg2} alt="профессиональное оборудование" />
+        </picture>
         <h3 className="subtitle">профессиональное оборудование</h3>
         <p>
           <span>87 тренажеров</span>
@@ -21,7 +32,10 @@ const Features = () => (
         </p>
       </li>
       <li className="pl pr">
-        <img src={img1} alt="квалифицированные тренеры" />
+        <picture className="about__image">
+          <source srcSet={imageWebp3} type="image/webp" />
+          <img src={imageJpg3} alt="квалифицированные тренеры" />
+        </picture>
         <h3 className="subtitle">квалифицированные тренеры</h3>
         <p>
           <span>15 тренеров</span>
