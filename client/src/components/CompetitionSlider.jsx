4d57f3c76ae484e3b5ccc6642d17866a3a5cf46f
E-Shangeraev <e.shangeraev@gmail.com>
2021-06-14 14:50:05 +0700

    Заканчиваю адаптив страницы Соревнования

--- client/src/components/CompetitionSlider.jsx
+++ client/src/components/CompetitionSlider.jsx
@@ -31,6 +31,22 @@ const CompetitionSlider = () => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   }
 
   const competitions = isLoaded
